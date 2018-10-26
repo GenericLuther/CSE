e000b87e54fca87beb954d5281a091a0d9a34fcf
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2018-10-26 10:40:02 -0700

    10/26/2018

--- notes/Pavline Luther - Mad Lib.py
+++ notes/Pavline Luther - Mad Lib.py
@@ -3,15 +3,26 @@
 #("Sports keep kids physically fit, engaged at school,")
 # ("and teach kids skills that will help them be successful in life.")
 
-high_official  = "Obama"
-common_people = "children"
-verb = "dabbing"
+recipient = "Obama"
+people = "turtles"
+# verb1 has to end in "ing" and the first letter has to be capitalized
+# verb2 is the same as verb 1 just not capitalized
+verb1 = "Eating mangos"
+verb2 = "eating mangos"
 place = "Aunt Bertha's house"
-adjective = "obese"
-ad
-something_you_can_be_in = "Pillsbury Doughboy"
+noun = "chunky monkey"
+issue = "living for a long of time"
+adjective1 = "obese"
+adjective2 = "dead"
+skill1 = "cosmology "
+skill2 = "freestyle dancing"
+skill3 = "martial arts"
+adjective_or_plural_noun = "astronauts"
 
-print("Dear %s, most %s would disagree that removing %s from %s is beneficial for %s." % (high_official, common_people, verb, place, common_people))
-print("It is clear that %s are good for %s overall." % (verb, common_people))
-print("%s keep %s %s, and engaged at %s," % (verb, common_people, adjective, place))
-print("and teach %s skills that will help them be successful in %s." % (common_people,something_you_can_be_in))
+# Story
+print("Dear %s, most %s would agree that %s from %s is beneficial for %s." % (recipient, people, verb2, place, people))
+print("%s can help %s into becoming a %s. %s prevents issues like %s." % (verb1, people, noun, verb1, issue))
+
+print("It is clear that %s is good for %s overall." % (verb2, people))
+print("%s keep %s %s, and %s at %s," % (verb1, people, adjective1, adjective2, place))
+print("and teach %s skills like: %s, %s, and %s. This will help them be %s." % (people, skill1, skill2, skill3, adjective_or_plural_noun))
