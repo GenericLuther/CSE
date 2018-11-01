8780e0d181516b80c1d66d435259f06b92fc1980
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2018-11-01 09:46:09 -0700

    11/1/2018

--- notes/Hello World.py
+++ notes/Hello World.py
@@ -137,3 +137,15 @@ print(your_grade)
 
 import random
 print(random.randint(0, 100))
+
+
+# Equality Statements
+print(5 > 3)
+print(5 >= 3)
+
+print(3 == 3)
+print (3 != 4)
+"""
+a = 3 # A is set to 3
+a == 3 # Is equal to 3?
+"""
