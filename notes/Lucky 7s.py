82d466d053e68c8941b7d8b147f8d6de5b0485f6
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2018-11-09 10:37:39 -0800

    11/9/2018

--- notes/Lucky 7s.py
+++ notes/Lucky 7s.py
@@ -3,13 +3,16 @@ money = int(15)
 playing = True
 d1 = random.randint(1,6)
 d2 = random.randint(1,6)
-while money > 0 and playing:
-    if d1+d2 == 7:
+roll= d1+d2
+while money > 0 :
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    roll = d1 + d2
+    if roll == 7:
         money += 4
-        print("%s" % money)
-    if d1+d2 > 7:
+        print(" You rolled a %s and a %s" % (d1, d2))
+        print("You have %s Weeb Coins left" % money)
+    else:
         money -= 1
-        print("%s" % money)
-    if d1+d2 < 7:
-        money -= 1
-        print("%s" % money)
+        print(" You rolled a %s and a %s" %(d1, d2))
+        print (" You have %s Weeb Coins left" % money)
