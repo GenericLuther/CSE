0d5fbee0b78d460612bdd11dbecf8a4bdb19f00d
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2018-11-28 09:46:57 -0800

    11/28/2018

---
+++ notes/Pavline Luther - Mad Lib 2.py
@@ -0,0 +1,30 @@
+# ("Dear superintendent, most students would disagree that removing sports from schools is beneficial for students.")
+# ("It is clear that sports are good for kids overall.")
+# ("Sports keep kids physically fit, engaged at school,")
+# ("and teach kids skills that will help them be successful in life.")
+
+# List
+words = []
+
+# Inputs
+words.append(input("Type in an person : "))
+words.append(input("Type in a plural noun : "))
+words.append(input("Type in a verb that is capitalized  : "))
+words.append(input("Type in the verb not capitalized : "))
+words.append(input("Type in a place : "))
+words.append(input("Type in a noun : "))
+words.append(input("Type in a issue : "))
+words.append(input("Type in a adjective : "))
+words.append(input("Type in a adjective : "))
+words.append(input("Type in a skill : "))
+words.append(input("Type in a skill : "))
+words.append(input("Type in a skill : "))
+words.append(input("Type in a adjective or profession : "))
+
+
+# Story
+print("Dear %s, most %s would agree that %s from %s is beneficial for %s." % (words[0], words[1], words[3]))
+print("%s can help %s into becoming a %s. %s prevents issues like %s." % (words[2], words[])
+print("It is clear that %s is good for %s overall." %
+print("%s keeps %s %s, and %s at %s," %
+print("and teach %s skills like: %s, %s, and %s. This will help them be %s." %
