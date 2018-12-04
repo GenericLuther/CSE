a6ccc1801cec92d7b9266b43fa202f2c4e068951
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2018-12-04 09:45:47 -0800

    12/4/2018

--- notes/Challenges.py
+++ notes/Challenges.py
@@ -5,3 +5,14 @@ def challenge1(firstname, lastname):
 
 print(challenge1('Luther', 'Pavline'))
 # 2.
+def challenge2(a):
+    if (a % 2 == 0):
+        print(a, "is an even number")
+    else:
+        print(a, "is an odd number")
+
+
+print(challenge2(3))
+# 3.
+def challenge3(h * b / 2)
+
