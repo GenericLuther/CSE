9137a94d39a26601c4726f2417802538cc27a79c
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-01-08 09:45:35 -0800

    1/8/2019

---
+++ Semester 2 notes.py
@@ -0,0 +1,26 @@
+print("Hello World")
+# Single Line Comment
+
+
+car = 5
+
+driving = True
+
+A = (3+2)
+
+print(" I have %s cars" % A)
+
+age = input("What is your age:")
+print("You are %s years old" % age)
+
+
+colors = ["Red", "Blue", "Yellow", "Green", "Purple"]
+colors.append("Orange")
+
+print(colors)
+
+colors.pop(0)
+
+print(colors)
+print(colors[0])
+print(len(colors))
