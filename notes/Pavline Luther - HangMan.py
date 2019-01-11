c5ace68c4be7d33e50ff2efe9dfd09b4c495d7c8
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-01-11 10:39:19 -0800

    1/11/2019

--- notes/Pavline Luther - HangMan.py
+++ notes/Pavline Luther - HangMan.py
@@ -1,10 +1,10 @@
 import random
 
 words = ["dog", "cat", "house", "home", "ball", "bay", "bat", "glass", "cable", "hand", "can", "mad"]
+answer = random.choice(words)
 guesses = 8
 
 while guesses > 0:
     print("This Hangman You Know The Rules")
-    input("You Have %d Wrong Guesses Left" % guesses)
-
-
+    letter = input("You Have %d Wrong Guesses Left: " % guesses)
+    print(letter)
