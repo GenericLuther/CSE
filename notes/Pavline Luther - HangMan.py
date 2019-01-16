55b87de238aa053b4afbd0c536d8ef65706ab5e6
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-01-16 09:44:33 -0800

    1/16/2019

--- notes/Pavline Luther - HangMan.py
+++ notes/Pavline Luther - HangMan.py
@@ -3,12 +3,29 @@ import random
 words = ["dog", "cat", "house", "home", "ball", "bay", "bat", "glass", "cable", "hand", "can", "mad"]
 answer = random.choice(words)
 guesses = 8
+letters = []
+if len(answer) is 3:
+    print()
 print("This Hangman You Know The Rules")
-if answer is "dog":
-    answer2 = "dog"    
-
 while guesses > 0:
-    letter = input("You Have %d Wrong Guesses Left: " % guesses)
+    print(letters)
+    print("You Have %d Wrong Guesses Left: " % guesses)
+    letter = input()
     print(answer)
     if letter in answer:
-        print(letter)
+        print()
+        letters.append(letter)
+    elif letter not in answer:
+        print("Nope")
+        guesses -= 1
+        letters.append(letter)
+
+
+
+
+
+
+
+
+
+
