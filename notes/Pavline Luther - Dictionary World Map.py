4cfa2619833c27cdecbbe123bc30a221132e48fb
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-02-07 09:46:16 -0800

    2/7/2019

--- notes/Pavline Luther - Dictionary World Map.py
+++ notes/Pavline Luther - Dictionary World Map.py
@@ -29,9 +29,10 @@ world_map_example = {
 de_dust2 = {
     "T_SPAWN": {
         "NAME": "T Spawn ",
-        "DESCRIPTION": "East is a ramp that leads to two more paths."
-                       "West leads to upper B."
-                       "North goes through a small alley way, going here would be suicide if they are prepared. ",
+        "DESCRIPTION": "You are in the T sides spawn."
+                       "East is a ramp that leads to two more paths"
+                       "West leads to upper B"
+                       "North goes through a small alley way, going here would be suicide if they are prepared ",
         "PATHS": {
             "EAST": 'OUTSIDE_LONG',
             "WEST": 'UPPER_B',
@@ -40,7 +41,8 @@ de_dust2 = {
     },
     "OUTSIDE_LONG": {
         "NAME": "Outside of Long",
-        "DESCRIPTION": "North of you is the entrance to long"
+        "DESCRIPTION": "You are outside of long, there are three paths"
+                       "North of you is the entrance to long"
                        "West of you is mid"
                        "South is T spawn",
         "PATHS": {
@@ -51,7 +53,7 @@ de_dust2 = {
     },
     "BLUE": {
         "NAME": "Blue",
-        "DESCRIPTION": "You are at blue bin"
+        "DESCRIPTION": "You are at blue bin."
                        "do you want to go north up long,"
                        "or do you want to go east into pit?",
         "PATHS": {
@@ -62,7 +64,7 @@ de_dust2 = {
     },
     "PIT": {
         "NAME": "Pit",
-        "DESCRIPTION": "You are in cover but there is only one way to go and that is north",
+        "DESCRIPTION": "You are in cover but there is only one way to go and that is north.",
         "PATHS": {
             "NORTH": 'LONG'
         }
@@ -70,9 +72,99 @@ de_dust2 = {
     "LONG": {
         "NAME": "Long",
         "DESCRIPTION": "This a long path up north to A site by going up ramp."
-                       "East of you is car for cover."
-                       "West of you is CT Ramp"
+                       "East of you is car for cover"
+                       "West of you is CT ramp"
                        "South of you goes to blue bin",
+        "PATHS": {
+            "EAST": 'CAR',
+            "WEST": 'CT_RAMP',
+            "NORTH": 'RAMP',
+            "SOUTH": 'BLUE'
+        }
+    },
+    "CAR": {
+        "NAME": "Car",
+        "DESCRIPTION": "You are behind cover which happens to be a car."
+                       "North of you is ramp"
+                       "South of you is long"
+                       "West of you is CT ramp",
+        "PATHS": {
+            "WEST": 'CT_RAMP',
+            "NORTH": 'RAMP',
+            "SOUTH": 'LONG'
+            }
+    },
+    "RAMP": {
+        "NAME": "Ramp",
+        "DESCRIPTION": "You are on a ramp right by A site"
+                       "North of you is Goose"
+                       "South is long"
+                       "West is A site",
+        "PATHS": {
+            "WEST": 'A_SITE',
+            "NORTH": 'GOOSE',
+            "SOUTH": 'LONG'
+        }
+    },
+    "GOOSE": {
+        "NAME": "Goose",
+        "DESCRIPTION": "You are at Goose, you got cover from cat"
+                       "South of you is A site",
+        "PATHS": {
+            "SOUTH": 'A_SITE'
+        }
+    },
+    "A_SITE": {
+        "NAME": "A Site",
+        "DESCRIPTION": "You have taken control of A site"
+                       "East of you is ramp"
+                       "West of you is A plat"
+                       "South you can jump off to get CT ramp"
+                       "North of you is Goose",
+        "PATHS": {
+            "EAST": 'RAMP',
+            "WEST": 'A_PLAT',
+            "NORTH": 'GOOSE',
+            "SOUTH": 'CT_RAMP'
+        }
+    },
+    "A_PLAT": {
+        "NAME": "A Plat",
+        "DESCRIPTION": "You are on A plat, you are in a open area."
+                       "South of you is short"
+                       "East of you is A site",
+        "PATHS": {
+            "EAST": 'A_SITE',
+            "SOUTH": 'SHORT'
+        }
+    },
+    "SHORT": {
+        "NAME": "Short",
+        "DESCRIPTION": "You are on short A"
+                       "South of you is cat that leads to catwalk"
+                       "If you jump towards East you will land on CT ramp"
+                       "North of you is A plat",
+        "PATHS": {
+            "EAST": 'CT_RAMP',
+            "NORTH": 'A_PLAT',
+            "SOUTH": 'CATWALK'
+        }
+    },
+    "CATWALK": {
+        "NAME": "Catwalk",
+        "DESCRIPTION": "You are on catwalk"
+                       "West of you is mid"
+                       "East of you is cat to short"
+                       "South goes to top of mid",
+        "PATHS": {
+            "EAST": 'SHORT',
+            "WEST": 'MID',
+            "SOUTH": 'TOP_MID'
+        }
+    },
+    "TOP_MID": {
+        "NAME": "",
+        "DESCRIPTION": "",
         "PATHS": {
             "EAST": '',
             "WEST": '',
@@ -80,6 +172,7 @@ de_dust2 = {
             "SOUTH": ''
         }
     }
+
 }
 # Controller
 playing = True
