60db3a496ffea30eea7c804f64a065da716bf8c0
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-02-13 09:45:54 -0800

    2/13/2019

--- notes/ObjectNotes.py
+++ notes/ObjectNotes.py
@@ -1,7 +1,10 @@
+import Special Random
+
+
 class Phone(object):
-    def__init__(self, carrier, charge_left):
-    # There are attributes that a phone has.
-    # There should all be relevant to our program
+    def __init__(self, carrier, charge_left):
+#  There are attributes that a phone has.
+#  There should all be relevant to our program
         self.screen = True
         self.camera = 2
         self.microphone = True
@@ -22,9 +25,9 @@ class Phone(object):
             if self.battery_left <= 0:
                 print("You can't. The phone is dead")
                 return
-            self.battery_left -= duration *  battery_loss_per_minute
-            if self self.battery_left < 0:
-                self. battery_left = 0
+                self.battery_left -= duration *  battery_loss_per_minute
+            if self.battery_left < 0:
+                self.battery_left = 0
                 print("You can't. The phone is dead")
                 return
             elif self.battery_left == 0:
@@ -32,6 +35,19 @@ class Phone(object):
             else:
                 print("You successfully make the phonecall.")
                 print("Your phone isnow at %s" % self.battery_left)
-
+    def smash_phone(self):
+        print("SMASH!!!!!!")
+        print("It broke.")
+        self.screen = False
 my_phone = Phone("ATT", 100)
 your_phone = Phone("bell", 0)
+default_phone = Phone("Verizon")
+
+my_phone.make_call(60)
+my_phone.make_call(10)
+my_phone.charge(100)
+my_phone.make_call(10)
+your_phone.smash_phone()
+your_phone.make_call(1)
+
+print(Special_Random.RandomWiebe.myrandom())
