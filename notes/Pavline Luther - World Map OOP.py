216052c95fa438bfb932693ee3eb4db9f6274b48
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-02-26 09:45:47 -0800

    2/26/2019

--- notes/Pavline Luther - World Map OOP.py
+++ notes/Pavline Luther - World Map OOP.py
@@ -8,6 +8,33 @@ class Room(object):
         self.northeast = northeast
         self.description = description
 
+class Player(object):
+    def __init__(self, starting_location):
+        self.current_location = starting_location
+        self.inventory = []
+
+    def find_next_room(self, direction):
+        """This method searches the current room to use if a room exists in that direction
+
+        :param direction: The direction that you want to move
+        :return: The room object if it exists, or move if it can
+        """
+        name_of_room = getattr(self.current_location, direction)
+        return globals()[name_of_room]
+
+    def move(self, new_location):
+        """
+
+        :param new_location: The room object of which you are in
+        """
+        self.current_location = new_location
+
+
+
+
+
+
+
 
 """
 #option 1 - Define as we go
@@ -33,16 +60,36 @@ long = Room("Long", None, 'ramp', 'blue', 'ct_ramp', None, "You're on a long pat
 ramp = Room("Ramp", None, None, 'long', 'a_site', None, "You're on a ramp, South is long and West is A SITE")
 a_site = Room("A SITE", 'ramp', None, 'ct_ramp', 'a_plat', None, "You're on A SITE, East is ramp, you can drop South "
                                                                  "onto CT ramp, and west is A plat")
-a_plat = Room("A Plat", 'a_site', None, 'cat', None, None, "")
-cat = Room("Cat", 'a_plat', None, 'top_mid','mid', None)
-top_mid = Room("Top of Mid", 'outside_long', 'mid', None, None, 'cat')
-mid = Room("Mid", 'cat', 'ct_mid', 'top_mid', 'lower_b', None)
-ct_mid = Room("CT Mid", 'CT_Spawn', None, 'mid', 'outside_b', None)
-CT_Spawn = Room("CT Spawn", 'ct_ramps', None, None, 'ct_mid', None)
-ct_ramp = Room("CT Ramp", 'CT_Spawn', None, None, 'long', None)
-outside_b = Room("Outside of B Site", 'ct_mid', 'window', None, 'b_site', None)
-window = Room("Window", 'b_site', None, 'outside_b', 'ct_mid', None)
-b_site = Room("B SITE", 'outside_b', None, 'upper_b', 'backplat', None)
-backplat = Room("Back Plat", None, None, 'upper_b', 'b_site', None)
-upper_b = Room("Upper B", 'lower_b', 'backplat', 'T_Spawn', None, None)
-lower_b = Room("Lower B", 'mid', None, None, 'upper_b', None)
+a_plat = Room("A Plat", 'a_site', None, 'cat', None, None, "You're on A plat, East is A SITE and South is cat")
+cat = Room("Cat", 'a_plat', None, 'top_mid','mid', None,"")
+top_mid = Room("Top of Mid", 'outside_long', 'mid', None, None, 'cat', "")
+mid = Room("Mid", 'cat', 'ct_mid', 'top_mid', 'lower_b', None,"")
+ct_mid = Room("CT Mid", 'CT_Spawn', None, 'mid', 'outside_b', None, "")
+CT_Spawn = Room("CT Spawn", 'ct_ramps', None, None, 'ct_mid', None, "")
+ct_ramp = Room("CT Ramp", 'CT_Spawn', None, None, 'long', None, "")
+outside_b = Room("Outside of B Site", 'ct_mid', 'window', None, 'b_site', None, "")
+window = Room("Window", 'b_site', None, 'outside_b', 'ct_mid', None, "")
+b_site = Room("B SITE", 'outside_b', None, 'upper_b', 'backplat', None, "")
+backplat = Room("Back Plat", None, None, 'upper_b', 'b_site', None, "")
+upper_b = Room("Upper B", 'lower_b', 'backplat', 'T_Spawn', None, None, "")
+lower_b = Room("Lower B", 'mid', None, None, 'upper_b', None, "")
+
+player = Player(T_Spawn)
+
+playing = True
+directions = ['north', 'south', 'east', 'west', 'northeast', 'up', 'down']
+
+while playing:
+    print(player.current_location.name)
+    print(current_node['DESCRIPTION'])
+    command = input(">_")
+    if command.lower() in ('q', 'quit', 'exit'):
+        playing = False
+    elif command.lower() in directions:
+        try:
+            room_name = current_node['PATHS'[command.upper()]]
+            curen_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way!")
+        else:
+            print("Command Not Found")
