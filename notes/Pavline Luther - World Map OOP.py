e457ab78eb5a05ab10bb04601ea459cf0dc5fec0
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-02-27 09:44:45 -0800

    2/27/2019

--- notes/Pavline Luther - World Map OOP.py
+++ notes/Pavline Luther - World Map OOP.py
@@ -8,6 +8,7 @@ class Room(object):
         self.northeast = northeast
         self.description = description
 
+
 class Player(object):
     def __init__(self, starting_location):
         self.current_location = starting_location
@@ -30,12 +31,6 @@ class Player(object):
         self.current_location = new_location
 
 
-
-
-
-
-
-
 """
 #option 1 - Define as we go
 R19A = Room("Mr Wiebe's Room")
@@ -53,17 +48,17 @@ T_Spawn = Room("T Spawn", 'outside_long', 'top_mid', None, 'upper_b', None, "You
 outside_long = Room("Outside of Long", None, 'blue', 'T_Spawn', 'top_mid', None, "You are outside of long, North of you"
                                                                                  "is blue, South goes to T Spawn, and "
                                                                                  "West goes to the top of mid")
-blue = Room("Blue Bin",'long', None, 'outside_long', None, None, "You're at blue, East of you is long, and South goes "
-                                                                 "outside of long.")
+blue = Room("Blue Bin", 'long', None, 'outside_long', None, None, "You're at blue, East of you is long, and South goes "
+                                                                  "outside of long.")
 long = Room("Long", None, 'ramp', 'blue', 'ct_ramp', None, "You're on a long path, North of you is ramp, South is blue,"
                                                            "and West is CT  ramp")
 ramp = Room("Ramp", None, None, 'long', 'a_site', None, "You're on a ramp, South is long and West is A SITE")
 a_site = Room("A SITE", 'ramp', None, 'ct_ramp', 'a_plat', None, "You're on A SITE, East is ramp, you can drop South "
                                                                  "onto CT ramp, and west is A plat")
 a_plat = Room("A Plat", 'a_site', None, 'cat', None, None, "You're on A plat, East is A SITE and South is cat")
-cat = Room("Cat", 'a_plat', None, 'top_mid','mid', None,"")
+cat = Room("Cat", 'a_plat', None, 'top_mid', 'mid', None, "")
 top_mid = Room("Top of Mid", 'outside_long', 'mid', None, None, 'cat', "")
-mid = Room("Mid", 'cat', 'ct_mid', 'top_mid', 'lower_b', None,"")
+mid = Room("Mid", 'cat', 'ct_mid', 'top_mid', 'lower_b', None, "")
 ct_mid = Room("CT Mid", 'CT_Spawn', None, 'mid', 'outside_b', None, "")
 CT_Spawn = Room("CT Spawn", 'ct_ramps', None, None, 'ct_mid', None, "")
 ct_ramp = Room("CT Ramp", 'CT_Spawn', None, None, 'long', None, "")
@@ -81,15 +76,15 @@ directions = ['north', 'south', 'east', 'west', 'northeast', 'up', 'down']
 
 while playing:
     print(player.current_location.name)
-    print(current_node['DESCRIPTION'])
+    print(player.current_location.description)
     command = input(">_")
     if command.lower() in ('q', 'quit', 'exit'):
         playing = False
     elif command.lower() in directions:
         try:
-            room_name = current_node['PATHS'[command.upper()]]
-            curen_node = world_map[room_name]
+            next_room = player.find_next_room(command)
+            current_location = next_room
         except KeyError:
             print("I can't go that way!")
-        else:
+    else:
             print("Command Not Found")
