4d28ec59bb3933d17e4045ab4643eff40340ce00
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-02-28 09:44:49 -0800

    2/28/2019

---
+++ notes/Inheritance.py
@@ -0,0 +1,59 @@
+
+#  field, method
+
+
+class Vehicle(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Car(Vehicle):
+    def __init__(self, name, mileage):
+        super(Car, self).__init__(name)
+        self.engine_statue = False
+        self.fuel = 100
+        self.mileage = mileage
+
+    def start_engine(self):
+        self.engine_statue = True
+        print("You have turned on the car.")
+
+    def move_forward(self):
+        if self.engine_statue is True:
+            self.fuel -= 1
+            print("The car moves forward.")
+        else:
+            print("You're car is off")
+
+    def turn_left(self):
+        self.fuel -= 1
+        print("The car turns left.")
+
+    def turn_off(self):
+        self.engine_statue = False
+        print("You turn off the engine.")
+
+
+class Impala(Car):
+    def __init__(self):
+        super(Impala, self).__init__('Impala', 25)
+
+
+class KeylessCar(Car):
+    def __init__(self):
+        super(KeylessCar, self).__init__(name,mileage)
+
+    def start_engine(self):
+        self.engine_statue = True
+        print("You push the button and the car turns on")
+
+
+wiebe_car = KeylessCar("Tesla", 125)
+
+
+jacob_car = Impala()
+jacob_car.start_engine()
+jacob_car.move_forward()
+jacob_car.turn_left()
+jacob_car.move_forward()
+jacob_car.turn_off()
