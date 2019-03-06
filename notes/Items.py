cb0a8ea6cbdc0b858816350fdd769f0f42cca438
GenericLuther <42385749+GenericLuther@users.noreply.github.com>
2019-03-06 09:44:56 -0800

    3/6/2019

--- notes/Items.py
+++ notes/Items.py
@@ -2,16 +2,65 @@ class Item(object):
     def __init__(self, name):
         self.name = name
 
+
 class Consumable(Item):
     def __init__(self, name):
         super(Consumable, self).__init__(name)
         self.name = name
+
+
 class Potion(Consumable):
     def __init__(self, name, heal, buff, debuff ):
         super(Potion, self).__init__(name)
-        heal = heal
-        buff = buff
-        debuff = debuff
+        self.heal = heal
+        self.buff = buff
+        self.debuff = debuff
+
+
+class Food(Consumable):
+    def __init__(self, name, hunger, heal, buff, debuff):
+        super(Food, self).__init__(name)
+        self.hunger = hunger
+        self.heal = heal
+        self.buff = buff
+        self.debuff = debuff
+
+
 class Weapon(Item):
     def __init__(self, name):
         super(Weapon, self).__init__(name)
+
+
+class Sword(Weapon):
+    def __init__(self, name, durability, damage, ability):
+        super(Sword, self).__init__(name)
+        self.durability = durability
+        self.damage = damage
+        self.ability = ability
+
+
+class Shield(Weapon):
+    def __init__(self, name, durability, damage, ability):
+        super(Shield, self).__init__(name)
+        self.durability = durability
+        self.damage = damage
+        self.ability = ability
+
+
+class Demonitizer(Weapon):
+    def __init__(self, name, ammo, damage, ability):
+        super(Demonitizer, self).__init__(name)
+        self.ammo = ammo
+        self.damage = damage
+        self.ability = ability
+
+
+class Throwable(Weapon):
+    def __init__(self, name, ammo, damage, ability):
+        super(Throwable, self).__init__(name)
+        self.ammo = ammo
+        self.damage = damage
+        self.ability = ability
+
+
+class Clothing(Item):
