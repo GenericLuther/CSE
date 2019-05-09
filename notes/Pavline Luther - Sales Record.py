2846f9c1cf1ec43fe296a6f1c065499e580b484e
Luther Pavline 6912 <arqe@fresnou.org>
2019-05-09 09:48:13 -0700

    5/9/2019

--- notes/Pavline Luther - Sales Record.py
+++ notes/Pavline Luther - Sales Record.py
@@ -3,11 +3,15 @@ import csv
 with open("Sales Records.csv", 'r') as profit_csv:
     print("Thinking...")
     reader = csv.reader(profit_csv)
+    firstline = True
     for row in reader:
+        if firstline:
+            firstline = False
+            continue
         profit = row[13]
         most_profit = float(0)
-        if profit == 'Total Profit':
-            pass
-        if float(profit) > most_profit:
+        if float(most_profit) < float(profit):
             most_profit = profit
-    print("The largest profit is: %d" % most_profit)
+        else:
+            continue
+    print("The largest profit is: %s" % most_profit)
