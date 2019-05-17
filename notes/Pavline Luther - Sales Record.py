d74b5aa2a4c6994e9efd0dba887e3a71c95871f1
Luther Pavline 6912 <arqe@fresnou.org>
2019-05-17 10:41:07 -0700

    5/17/2019

--- notes/Pavline Luther - Sales Record.py
+++ notes/Pavline Luther - Sales Record.py
@@ -3,40 +3,16 @@ total_profit = float(0)
 average_profit = float(0)
 average_profit_item = ""
 total_profit_item = ""
-items = []
-
-
-def products():
-    firstline = True
-    for row in reader:
-        if firstline is True:
-            firstline = False
-            continue
-        if row[2] in items:
-            continue
-        else:
-            items.append(row[2])
-    return items
-
-
-def profit():
-    firstline = True
-    for row in reader:
-        if firstline is True:
-            firstline = False
-            continue
-        else:
-            if float(row[13]) > total_profit:
-                total_profit_1 = float(row[13])
-                profitable_item = row[2]
-                print(profitable_item)
-                print(total_profit_1)
-
+items = ['Fruits', 'Clothes', 'Meat', 'Beverages', 'Office Supplies', 'Cosmetics', 'Snacks', 'Personal Care',
+         'Household', 'Vegetables', 'Baby Food', 'Cereal']
+firstline = True
 
 with open("Sales Records.csv", 'r') as profit_csv:
     print("Thinking...")
     reader = csv.reader(profit_csv)
 
     for row in reader:
-        products()
-        profit()
+        if row[2] == 'Fruits':
+            if float(row[13]) > float(total_profit):
+                total_profit = row[13]
+    print(total_profit)
